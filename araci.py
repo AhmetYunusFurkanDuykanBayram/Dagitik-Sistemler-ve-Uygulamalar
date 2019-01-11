71a79f290f6b2e2899f1612c3d8e2ac3b1024b9b
ahmetarca <45852249+ahmetarca@users.noreply.github.com>
2019-01-11 11:12:53 +0300

    Add files via upload
    aracının, loglama ve diğer dosyalama işlemlerinin eklenmiş son hali

--- araci.py
+++ araci.py
@@ -1,14 +1,113 @@
-import queue
+﻿import queue
 import threading
 import time
 import socket
 from uuid import getnode as get_mac
-UUID = get_mac()
+from pathlib import Path 
+from time import sleep
+from Crypto.PublicKey import RSA
+from Crypto import Random
+from Crypto.Hash import SHA256
+from os import listdir
+import random
 
+#UUID = get_mac()
 ExitFlag = False
 
-liste = {str(UUID): [str(UUID), "192.168.0.14", "12345", "A", "araci1"]}
+UUID_file = Path("UUID.txt")
+#Eger daha once UUID olusturulmussa
+if UUID_file.is_file():
+	f = open("UUID.txt", "r")
+	f2 = open("port.txt", "r")
+	f3 = open("nickname.txt", "r")
+	UUID = f.read()
+	port = int(f2.read())
+	nickname = f3.read()
+	f.close();
+	f2.close();
+	f3.close();
+#Degilse yeni olusturulani dosyaya yaz
+else:
+	nickname = input("Nickname Giriniz: ")
+	#Yeni UUID olusturma
+	UUID = int(random.random()*10000)
+	UUID = str(UUID)
+	#server icin port (2000-65500)
+	port = int(random.random()*63500)+2000
+	f = open("UUID.txt", "w")
+	f2 = open("port.txt", "w")
+	f3 = open("nickname.txt", "w")
+	f.write(UUID)
+	f2.write(str(port))
+	f3.write(nickname)
+	f.close();
+	f2.close();
+	f3.close();
+	
+#ip adresini ogrenme
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+ip = s.getsockname()[0]
+buf_size = 2048
 
+#listeler ve dictionary'ler
+liste = {UUID: [UUID, ip, str(port), "A", nickname]}
+
+liste_file = Path("liste.txt")
+if liste_file.is_file():
+	f = open("liste.txt", "r")
+	txt = f.read()
+	f.close();
+	ls = txt.split('\n')
+	while True:
+		try:
+			ls.remove('')
+		except:
+			break
+	for i in ls:
+		j = i.split(', ')
+		liste[j[0]] = j
+
+else:
+	f = open("liste.txt", "w")
+	f.write(liste[UUID][0] + ", " + liste[UUID][1] + ", " + liste[UUID][2] + ", " + liste[UUID][3] + ", " + liste[UUID][4] + '\r\n')
+	f.close();
+
+print("liste:")
+for i in liste:
+	print("  ", liste[i])
+
+log_file = Path("log.txt")
+if not log_file.is_file():
+	f = open("log.txt", "w")
+	f.close();
+
+
+class loggerThread (threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+
+	def run(self):
+		f = open("log.txt", "a")
+		f.write(str(time.ctime()) + " - " + "Starting." + '\r\n')
+		f.close();
+		
+		while True:
+			try:
+				msg = logQueue.get()
+			except:
+				msg = "QUIT"
+			if msg == "QUIT":
+				f = open("log.txt", "a")
+				f.write(str(time.ctime()) + " - " + "QUIT received." + '\r\n')
+				f.close();
+				break
+			f = open("log.txt", "a")
+			f.write(str(time.ctime()) + " - " + str(msg) + '\r\n')		#print yerine dosyaya yaz.
+			f.close();
+		f = open("log.txt", "a")
+		f.write(str(time.ctime()) + " - " + "Exiting." + '\r\n')
+		f.close();
 
 def parser(data):
 	if "\r\n" in data:
@@ -18,11 +117,16 @@ def parser(data):
 	komut = data[:4]
 	icerik = data[5:]
 	list = icerik.split(", ")
+	if komut == "ERSY" or komut == "ERLO" or komut == "ERKY":
+		print(komut)
 	return komut, list
 
+
 def soketeYaz(c, text):
+	sleep(0.05)
 	c.send((text+"\r\n").encode())
-	print(text,"gonderildi")
+	logQueue.put(text + " gonderildi")	#print yerine logger threade yollanacak
+
 
 class serverThread(threading.Thread):
 	def __init__(self, c, addr):
@@ -32,13 +136,15 @@ class serverThread(threading.Thread):
 		
 	def run(self):
 		UUID_C = None
-		accespted = False
+		accepted = False
 		while True:
 			ERSY = False
 			try:
-				data = self.c.recv(1024).decode()
+				data = self.c.recv(buf_size).decode()
 				if len(data) == 0:	#karsi taraf Ctrl C ile kapatirsa bos string okuyor
 					break
+				logQueue.put(data[:-2] + " alindi")
+				
 			except:
 				break
 			komut, icerik = parser(data)
@@ -54,7 +160,7 @@ class serverThread(threading.Thread):
 					soketeYaz(self.c, "ERSY")
 					ERSY = True
 				
-				if not ERSY:
+				if not ERSY:	#syntax hatasi yoksa
 					soketeYaz(self.c, "WAIT")
 					try:
 						tmp = liste[UUID_C]
@@ -62,31 +168,32 @@ class serverThread(threading.Thread):
 						tmp=["","","","",""]
 					if tmp[0] == icerik[0] and tmp[1] == icerik[1] and tmp[2] == icerik[2]:
 						soketeYaz(self.c, "ACCT")
-						accespted = True
+						accepted = True
 					else:
 						try:
 							s = socket.socket()
 							s.connect((adres,int(port)))
 							soketeYaz(s, "WHOU")
-							data = s.recv(1024).decode()
-							print(data)
+							data = s.recv(buf_size).decode()
+							logQueue.put(data[:-2] + " alindi")
 							s.close()
 							
 							komut, icerik2 = parser(data)
 							if komut == "MYID":
 								if icerik2[0] == UUID_C:
 									soketeYaz(self.c, "ACCT")
-									accespted = True
+									accepted = True
 									liste[UUID_C]=icerik	#listeye ekleme
+									f = open("liste.txt", "a")
+									f.write(liste[UUID_C][0] + ", " + liste[UUID_C][1] + ", " + liste[UUID_C][2] + ", " + liste[UUID_C][3] + ", " + liste[UUID_C][4] + '\r\n')
+									f.close()
 								else:
 									soketeYaz(self.c, "REJT")
-								
-
 						except:
 							soketeYaz(self.c, "REJT")
 			elif komut == "WHOU":
 				soketeYaz(self.c, "MYID " + str(UUID))
-			elif not accespted:
+			elif not accepted:
 				soketeYaz(self.c, "ERLO")
 			elif komut == "LIST" and len(icerik)==1:
 				size = 0
@@ -96,18 +203,22 @@ class serverThread(threading.Thread):
 					try:
 						size = int(icerik[0])
 					except:
-						soketeYaz(self.c, "ERSY")	#int bir deger degilse
+						soketeYaz(self.c, "ERSY int")	#int bir deger degilse
+						continue
 				
 				for l in liste:
 					if size <=0:
 						break
 					soketeYaz(self.c, "LSIS " + liste[l][0] + ", " + liste[l][1] + ", " + liste[l][2] + ", " + liste[l][3] + ", " + liste[l][4])
 					size = size-1
-	
+				soketeYaz(self.c, "LSIS")
+
 			else:
 				soketeYaz(self.c, "ERSY")
+
 		self.c.close()
 		print("baglanti kapatildi")
+		logQueue.put("baglanti kapatildi")
 
 
 queueLock = threading.Lock()
@@ -117,7 +228,11 @@ logQueue = queue.Queue()
 
 s = socket.socket()  # Create a socket object
 host = "0.0.0.0"  # Accesible by all of the network
-port = 12345  # Reserve a port for your service.
+
+
+thread = loggerThread()
+thread.start()
+
 
 try:
 	s.bind((host, port))  # Bind to the port
